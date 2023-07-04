1ca8ebddbebb5e5ae554e55afc87048439d2adf8
Karol-64bit <ambalabamba42@gmail.com>
2023-07-04 17:34:38 +0200

    mvp

--- screens/GenerateRaport.js
+++ screens/GenerateRaport.js
@@ -1,22 +1,76 @@
 import { useNavigation } from '@react-navigation/core'
 import React, { useEffect, useState } from 'react'
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { auth } from '../firebase'
-import {  createUserWithEmailAndPassword, signInWithEmailAndPassword  } from 'firebase/auth';
+import { collection, query, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import * as Print from 'expo-print';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing';
+
+
 
 const GenerateRaport = () => {
 
+    const [items, setItems] = useState([]);
+
+  const fetchNotes = async () => {
+    const user = auth.currentUser;
+    const userId = user.uid;
+
+    const notesQuery = query(collection(db, 'users', userId, 'items'));
+
+    try {
+      const querySnapshot = await getDocs(notesQuery);
+
+      const fetchedNotes = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      setItems(fetchedNotes);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
   const navigation = useNavigation()
 
-  const handleGenerateRaport = () => {
+  const handleGenerateRaport = async () => {
 
-    
+        try {
+          const pdfContent = [];
+      
+          for (const data of items) {
+            const text = `Title: ${data.title}\nContent: ${data.content}\Amount: ${data.amount}`;
+            pdfContent.push(text);
+          }
+      
+          const htmlContent = pdfContent.join('<br>');
+
+          const { uri } = await Print.printToFileAsync({ html: htmlContent });
+      
+          const fileName = 'raport.pdf';
+          const fileUri = `${FileSystem.documentDirectory}${fileName}`;
+      
+          await FileSystem.moveAsync({
+            from: uri,
+            to: fileUri,
+          });
+      
+          await Sharing.shareAsync(fileUri);
+      
+          console.log('PDF file has been saved and shared:', fileUri);
+        } catch (error) {
+          console.error('An error occurred while generating and saving the PDF file:', error);
+        }
+
 
     navigation.replace("Home");
   }
-
-
 
   return (
     <KeyboardAvoidingView
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,6 +10,7 @@ const LoginScreen = () => {
   //  Karol@wsiz.pl
   //  test123
 
+
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
